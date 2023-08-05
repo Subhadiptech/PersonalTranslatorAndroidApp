25c83fe1229e31a0036a95b8d009dc5764479d99
Subhadiptech <subhadipdhn@gmail.com>
2023-08-05 17:42:02 +0530

    bottom sheet to select language done

--- app/src/main/java/com/ersubhadip/mypersonaltranslator/core/AppModules.kt
+++ app/src/main/java/com/ersubhadip/mypersonaltranslator/core/AppModules.kt
@@ -1,11 +1,11 @@
 package com.ersubhadip.mypersonaltranslator.core
 
-import com.ersubhadip.mypersonaltranslator.presentation.home.TextToVoiceViewModel
+import com.ersubhadip.mypersonaltranslator.presentation.home.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val globalModules = module {
     viewModel {
-        TextToVoiceViewModel()
+        HomeViewModel()
     }
 }
--- app/src/main/java/com/ersubhadip/mypersonaltranslator/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/ersubhadip/mypersonaltranslator/presentation/home/HomeViewModel.kt
@@ -3,16 +3,32 @@ package com.ersubhadip.mypersonaltranslator.presentation.home
 import android.content.Context
 import android.speech.tts.TextToSpeech
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import java.util.Locale
 
-class TextToVoiceViewModel : ViewModel() {
+class HomeViewModel : ViewModel() {
 
     private var textToSpeech: TextToSpeech? = null
     private val _isSuccess = MutableStateFlow(false)
     val isSuccess: StateFlow<Boolean> = _isSuccess
 
+    fun getLanguages(): List<String> = listOf(
+        "English",
+        "English(US)",
+        "English(UK)",
+        "Canada",
+        "French",
+        "German",
+        "Spanish",
+        "Chinese",
+        "Simplified Chinese",
+        "Italian",
+        "Japanese",
+        "Korean"
+    )
 
     fun textToSpeech(text: String, lang: Locale, context: Context) {
         textToSpeech = TextToSpeech(
@@ -20,6 +36,7 @@ class TextToVoiceViewModel : ViewModel() {
         ) { status ->
             when (status) {
                 TextToSpeech.SUCCESS -> {
+                    viewModelScope.launch { _isSuccess.emit(true) }
                     textToSpeech?.let { txtToSpeech ->
                         txtToSpeech.language = lang
                         txtToSpeech.setSpeechRate(1.0f)
@@ -33,7 +50,7 @@ class TextToVoiceViewModel : ViewModel() {
                 }
 
                 TextToSpeech.ERROR -> {
-
+                    viewModelScope.launch { _isSuccess.emit(false) }
                 }
             }
         }
